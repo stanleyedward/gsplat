c7e2b8b104ee7770a7f1685d59944033f382f43d
stanleyedward <114278820+stanleyedward@users.noreply.github.com>
2025-05-04 17:46:06 +0530

    modsetup

--- install.sh
+++ install.sh
@@ -2,21 +2,20 @@ conda create -n gsplat python=3.10 -y
 conda activate gsplat
 conda install conda-forge::gxx=9.5.0 -y
 conda install cuda -c nvidia/label/cuda-11.8.0 -y
-pip install torch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 --index-url https://download.pytorch.org/whl/cu118
+# pip install torch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 --index-url https://download.pytorch.org/whl/cu118
+conda install pytorch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1  pytorch-cuda=11.8 -c pytorch -c nvidia
 #install gsplat
 pip install ninja numpy jaxtyping rich wheel packaging
 pip install gsplat --index-url https://docs.gsplat.studio/whl/pt24cu118
 
+# Set CUDA_HOME to your conda environment
+export CUDA_HOME=$CONDA_PREFIX
+# Update LD_LIBRARY_PATH with the specific CUDA lib paths
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$CONDA_PREFIX/lib/stubs:$LD_LIBRARY_PATH
+python "$@"
+
 #install cuda (need this for examples)
 pip install -r examples/requirements.txt
-
-### from the original 3dgs setup
-conda create -n gs python
-conda activate python
-conda install conda-forge::gxx=9.5.0
-conda install cuda -c nvidia/label/cuda-12.4.1
-pip3 install torch torchvision torchaudio black
-### torch and torch audio==2.6.0 torchvision==0.21.0
 
 sudo pacman -S \
   base-devel \
@@ -32,8 +31,4 @@ sudo pacman -S \
   embree \ 
   lib32-glm
 
-pip install --no-build-isolation submodules/simple-knn submodules/diff-gaussian-rasterization submodules/fused_ssim
 
-### FOR gsplat repo only
-pip install ninja numpy jaxtyping rich wheel packaging
-pip install -r examples/requirements.txt
