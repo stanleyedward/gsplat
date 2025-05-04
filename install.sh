25f7d3f475323e65f771a6db0caf98e998c8bc74
stanleyedward <114278820+stanleyedward@users.noreply.github.com>
2025-05-04 11:43:18 +0530

    setup script

---
+++ install.sh
@@ -0,0 +1,8 @@
+pip install torch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 --index-url https://download.pytorch.org/whl/cu118
+#install gsplat
+pip install ninja numpy jaxtyping rich
+pip install gsplat --index-url https://docs.gsplat.studio/whl/pt24cu118
+
+#install cuda (need this for examples)
+conda install cuda -c nvidia/label/cuda-11.8.0
+
