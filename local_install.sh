64492e81584fba05076bf970f75e918dcb9e3fa2
stanley <114278820+stanleyedward@users.noreply.github.com>
2025-05-08 11:55:04 +0530

    Update local_install.sh

--- local_install.sh
+++ local_install.sh
@@ -2,7 +2,6 @@ conda create -n gsplat python=3.10 -y
 conda activate gsplat
 conda install conda-forge::gxx=9.5.0 -y
 conda install cuda -c nvidia/label/cuda-11.8.0 -y
-# pip install torch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 --index-url https://download.pytorch.org/whl/cu118
 conda install pytorch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1  pytorch-cuda=11.8 -c pytorch -c nvidia
 #install gsplat
 pip install ninja numpy jaxtyping rich wheel packaging
